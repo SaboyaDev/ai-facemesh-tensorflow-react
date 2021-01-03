5d4320f5f0794f4739c41547423bf81461c89aa1
SaboyaDev <saboyadev@gmail.com>
2021-01-03 16:01:44 -0500

    Add .eslincache to the .gitignore. I was able to set up the webcam and canvas componenets inside the app component and have webCam component and canvas display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+// - Install dependencies DONE
+// - Import dependencies DONE
+// - Setup Webcam and canvas DONE
+// - Define references to the webcam and canvas DONE
+// - Load Facemesh
+// - Detect function
+// - Drawing utilities
+// - Load triangulation
+// - Setup triangle path
+// - Setup point drawing
+// - Add drawMesh to detect function
+
+// useRef allows to have a reference to our
+// canvas component as well as our webcam
+import React, { useRef } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as facemesh from '@tensorflow-models/facemesh';
+import WebCam from 'react-webcam';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Setup references
+	const webcamRef = useRef(null);
+	const canvasRef = useRef(null);
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<WebCam ref={webcamRef} className='webcam' />
+				<canvas ref={canvasRef} className='canvas' />
+			</header>
+		</div>
+	);
 }
 
 export default App;
